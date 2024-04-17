7159dbef5a92195484de54ea12088935afa71157
Sevastyan Savanyuk <sevasnex@gmail.com>
2024-04-17 12:40:08 +0200

    Auto-generate dart package project

---
+++ example/google_oauth2_example.dart
@@ -0,0 +1,6 @@
+import 'package:google_oauth2/google_oauth2.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/google_oauth2_test.dart
@@ -0,0 +1,16 @@
+import 'package:google_oauth2/google_oauth2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
